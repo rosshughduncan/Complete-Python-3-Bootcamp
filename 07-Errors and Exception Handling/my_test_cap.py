09443947e31bb4bc549d74720ef5d71e3447e12e
rosshughduncan <62666653+rosshughduncan@users.noreply.github.com>
2020-05-04 23:02:36 +0100

    Finished section 7

---
+++ 07-Errors and Exception Handling/my_test_cap.py
@@ -0,0 +1,16 @@
+import unittest
+import mycap
+
+class TestCap(unittest.TestCase):
+    def test_one_word(self):
+        text = 'python'
+        result = mycap.cap_text(text)
+        self.assertEqual(result, 'Python')
+
+    def test_multiple_words(self):
+        text = 'monty python'
+        result = mycap.cap_text(text)
+        self.assertEqual(result, 'Monty Python')
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ 07-Errors and Exception Handling/myfile.py
@@ -0,0 +1,18 @@
+'''
+A very simple script
+'''
+def myfunc():
+    '''
+    A simple function
+    '''
+    first = 1
+    second = 2
+    print(first)
+    print(second)
+
+myfunc()
+
+# a = 1
+# b = 2
+# print(a)
+# print(B)
